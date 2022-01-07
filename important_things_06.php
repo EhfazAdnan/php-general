12ea7986f1ecef2b5b19369b0a520d9175cc6700
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-01-07 08:34:34 -0800

    remove element from array

---
+++ important_things_06.php
@@ -0,0 +1,23 @@
+<?php
+   // delay execuition
+   echo 'hello World';
+
+   $value  = 1;
+
+   if($value == 0){
+    sleep(5);
+    echo 'After Sleep function runs';
+   }
+   echo "<br/>";
+
+
+
+   // delete an element from array
+   $x = array(1,2,3,4,5);
+   var_dump($x);
+   echo "<br/>";
+   unset($x[3]);
+   var_dump($x);
+   echo "<br/>";
+
+?>
