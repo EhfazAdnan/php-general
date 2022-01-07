cb6c212a9442aeaa5ad22e0c977857240b6de967
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-01-07 05:19:44 -0800

    check http or https

--- site_url_protocol_02.php
+++ site_url_protocol_02.php
@@ -2,5 +2,14 @@
   $url = 'https://www.youtube.com/watch?v=_cogTC-RfAg&list=PL9-9CrcmxdyMIVf8zSk33GYIvSmaHX6tr';
   $url = parse_url($url);
   echo 'Schema : ' . $url['scheme'];
+  echo "<br/>";
+
+  // check is http or https
+
+  if(!empty($_SERVER['HTTPS'])){
+    echo 'HTTPS';
+  }else{
+    echo 'HTTP';
+  }
 
 ?>
