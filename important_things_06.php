84196a18725afac4013baf3c1f8e022325e48781
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-01-08 08:30:56 -0800

    print number series

--- important_things_06.php
+++ important_things_06.php
@@ -88,4 +88,46 @@
    }
    echo "<br/>";
 
-?>
+
+   // find day differnece between 2 dates
+   $to_date = time();
+   $from_date = strtotime("2012-01-31");
+   $day_diff = $to_date - $from_date;
+   echo floor($day_diff / (60*60*24));
+   echo "<br/>";
+   echo "<br/>";
+
+   // create table useing loop
+ 
+   ?>
+
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Document</title>
+   </head>
+   <body>
+       <table align="left" border="1" cellpadding="3" cellspacing="0">
+           <?php
+              for($i = 1; $i <= 10; $i++){
+                  echo "<tr>";
+                  for($j = 1; $j <= 10; $j++){
+                    echo "<td>$i * $j = ".$i * $j."</td>";
+                  }
+              }
+              echo "<tr/>";
+           ?>
+       </table>
+   </body>
+   </html>
+
+   <?php
+   
+   // insert hypen
+
+   
+   
+   ?>
