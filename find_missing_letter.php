e946026ad6d189ba202d983fe2b26c5b98eb27e5
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-06-06 03:35:23 -0700

    solved find missing letter problem

--- find_missing_letter.php
+++ find_missing_letter.php
@@ -1,1 +1,29 @@
-<?php echo 'hello world'; ?>
+<?php
+
+function find_missing_numbers($getvalues){
+  
+    $alpha = '';
+
+    foreach(range("a","z") as $letters){
+        $alpha .= $letters;
+    }
+
+    $start = strpos($alpha, $getvalues[0]);
+
+    for($i = 0; $i < strlen($getvalues); $i++){
+
+        // echo $alpha[$start + $i];
+        // echo $getvalues[$i];
+        // echo "<br/>";
+
+        if($getvalues[$i] !== $alpha[$start + $i]){
+            return $alpha[$start + $i];
+        }
+    }
+
+    return 'no mismatch';
+}
+
+echo find_missing_numbers("bcdfghj");
+echo "<br/>";
+echo find_missing_numbers("abcd");
