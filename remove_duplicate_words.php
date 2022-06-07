89c24228316b8f95fe81c9d58fa9cd15c9394c0d
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-06-07 11:54:18 -0700

    remove duplicate words

---
+++ remove_duplicate_words.php
@@ -0,0 +1,15 @@
+<?php
+
+function remove_duplicate_words($word){
+
+    // convert to array
+
+    // remove duplicate words
+
+    // convert array to sentence
+}
+
+echo remove_duplicate_words("hello hello world");
+
+// echo "hello hello world"
+// output: "hello world"
